ab1d5f8f6ab5bdba42755c3a4286d63e5902a5e3
JudehAlaaraj <109723372+JudehAlaaraj@users.noreply.github.com>
2022-07-28 11:15:03 +0300

    g

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,11 +4,25 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
-def home():
-	
-	return render_template('home.html')
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+	if request.method == 'POST':
+		quote = request.form['quote']
+		name = request.form['name']
+		age= request.form['age']
+		login_session['quote'] = quote
+		login_session['name'] = name
+		login_session['age'] = age
+		#if 'age' in login_session and 'name' in login_session and 'quote' in login_session:
+		try:
+			a = login_session['quote']
+			b = login_session['name']
+			c = login_session['age']
+			return redirect(url_for('thanks'))
+		except: 
+			return redirect(url_for('error'))
+	return render_template("home.html")
 
 @app.route('/error')
 def error():
@@ -19,7 +33,7 @@ def error():
 @app.route('/display')
 def display():
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html', age=login_session['age'], name=login_session['name'] , quote=login_session['quote']) # What variables are needed?
 
 
 @app.route('/thanks')
